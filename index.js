6aa62d9c74e4cc7d2fef0357d4bbaabb9683a0cf
Austin <austinpulley19@yahoo.com>
2022-03-20 11:45:11 -0500

    add functions on load, add hearts variables for likes

--- index.js
+++ index.js
@@ -1,53 +1,84 @@
 //DOMContentLoaded on page load
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed')
 });
+getWorkouts()
+addEventListeners()
 
 //Fetching the API
-fetch("https://exercisedb.p.rapidapi.com/exercises", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "exercisedb.p.rapidapi.com",
-		"x-rapidapi-key": "3dea8b2111msh4b6e81c8cbe1d41p1b3bffjsn3e1a1aa2c720"
-	}
-})
-.then(response => response.json())
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+let exercises
 
-//variables
-const workoutLists= document.querySelector('#workoutLists')
-const searchBarSubmit = document.getElementById('searchBar')
-let finishedSearch = [];
-
-//search bar make an array then filter through and show results matching on submit
-searchBarSubmit.addEventListener('submit', function(e){
-    const searchInput = e.target.value.toLowerCase();
-    const filterSearch = finishedSearch.filiter((workout)=> {
-        return workout.name.toLowerCase().includes(searchInput)
+function getWorkouts() {
+  const url = "https://exercisedb.p.rapidapi.com/exercises"
+	fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+            "x-rapidapi-key": "3dea8b2111msh4b6e81c8cbe1d41p1b3bffjsn3e1a1aa2c720"
+        }
     })
-    displaySearch(filterSearch)
-})
-console.log(searchBarSubmit)
-//make object a string 
-const displaySearch = (workout) => {
-    const htmlString = workout
-        .map((workouts) => {
-            return `
-            <li class="workouts">
-                <h2>${workouts.name}</h2>
-                <h2>${workouts.bodyPart}</h2>
-                <h2>${workouts.equipment}</h2>
-                <h2>${workouts.target}</h2>
-            </li>
-        `;
-        })
-        .join('');
-    workoutLists.innerHTML = htmlString;
-};
+		.then(res => {
+			return res.json() 
+		})
+		.then(data => {
+			exercises = data
+			console.log(exercises)
+		})
+}
+
+//Drop down to select body part submit, create array.
+function addEventListeners() {
+  const bodyDrop = document.getElementById('submit')
+
+  bodyDrop.addEventListener('click', () => {
+      const body = document.getElementById('bodyPart-dropdown')
+      const selectedPart = [body].map(option => option.value)
+      console.log(selectedPart)
+      renderParts(selectedPart) 
+  })
+}
+//render the body parts add like functionality
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+
+function renderParts(selectedPart) {
+  console.log("WORKOUT")
+    const workoutDiv = document.getElementById('workouts')
+    workoutDiv.innerHTML = ' '
+    console.log(selectedPart)
+    let filteredParks = parks.data.filter(el => {
+            return el.states.includes(selectedPart)
+        }
+    )
+    console.log(filteredParks)
+
+    for (let i = 0; i < filteredParks.length; i++) {
+            const ul = document.createElement('ul')
+            const img = document.createElement('img')
+            const descrip = document.createElement('p')
+            const directions = document.createElement('a')
+            const liker = document.createElement('button')
+            liker.textContent = EMPTY_HEART
+            directions.innerText = 'Get Directions'
+            directions.href = filteredParks[i].directionsUrl
+            directions.target = '_blank'
+            img.src = filteredParks[i].images[0].url
+            ul.innerText = filteredParks[i].fullName
+            descrip.innerText = filteredParks[i].description
+            workoutDiv.append(img, ul, descrip, directions, liker) 
+            liker.addEventListener('click', likePark)
+            }
+        }  
+  
+
+  const likePark = (e) => {
+    const liker = e.target
+    const like = liker.textContent
+    if(like===EMPTY_HEART) {
+      liker.textContent = FULL_HEART
+    } else {
+      liker.textContent = EMPTY_HEART
+    }
+  }
 // mouseOver
 // mouseLeave
